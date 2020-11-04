15efb871c6516dc37dcdced66f304c763108c1c6
Michael Corey <mikejcorey@gmail.com>
2020-11-04 10:36:08 -0600

    flat prez pusher for cron

--- build_electoral_map_print.sh
+++ build_electoral_map_print.sh
@@ -8,8 +8,8 @@ cat <(curl -s $ELEX_S3_URL/json/results-national-ap-latest.json) | \
   ndjson-filter 'd.level === "state" && d.officename === "President" && ["Biden", "Trump"].indexOf(d.last) != -1' | \
   ndjson-reduce '(p[d.statepostal] = p[d.statepostal] || []).push({name: d.last, votecount: d.votecount, votepct: d.votepct, bool_winner: d.winner}), p' '{}' | \
   ndjson-split 'Object.keys(d).map(key => ({statepostal: key, votes: d[key]}))' | \
-  ndjson-map '{"statepostal": d.statepostal, "votes": d.votes.filter(obj => obj.name != "").sort((a, b) => b.votecount - a.votecount)}' | \
-  ndjson-map '{"statepostal": d.statepostal, "leader": d.votes.every(o => o.votecount == 0) ? "no-votes" : d.votes[0].votecount != d.votes[1].votecount ? d.votes[0].name : "even", "winner_or_leading": d.votes.every(o => o.votecount == 0) ? "no-votes" : d.votes[0].votecount != d.votes[1].votecount && d.votes[0].bool_winner == true ? d.votes[0].name + "_winner" : d.votes[0].votecount != d.votes[1].votecount && d.votes[0].bool_winner == false ? d.votes[0].name + "_leader" : "even", "winner_margin": (d.votes[0].votepct - d.votes[1].votepct).toFixed(2)}' | \
+  ndjson-map '{"statepostal": d.statepostal, "votes": d.votes.filter(obj => obj.name != "").sort((a, b) => b.bool_winner - a.bool_winner).sort((a, b) => b.votecount - a.votecount)}' | \
+  ndjson-map '{"statepostal": d.statepostal, "leader": d.votes.every(o => o.votecount == 0) ? "no-votes" : d.votes[0].votecount != d.votes[1].votecount ? d.votes[0].name : "even", "winner_or_leading": d.votes[0].bool_winner == true ? d.votes[0].name + "_winner"  : d.votes.every(o => o.votecount == 0) ? "no-votes" : d.votes[0].votecount != d.votes[1].votecount && d.votes[0].bool_winner == false ? d.votes[0].name + "_leader" : "even", "winner_margin": (d.votes[0].votepct - d.votes[1].votepct).toFixed(2)}' | \
   ndjson-join --left 'd.statepostal' 'd.abbreviation' - <(cat json/state-electoral-votes-and-history.json | jq -c ".[]") | \
   ndjson-map 'Object.assign(d[0], d[1])' > spatial/prez_state_winners.tmp.ndjson
 
@@ -29,7 +29,7 @@ topo2geo states=- < spatial/states-electoral-final-topo.json | \
 # Colorizing SVG with leader/winner dilineated
 mapshaper spatial/states-electoral-final-geo.json \
   -quiet \
-  -colorizer name=calcFill colors='#115E9B,#CFCFCF,#AE191C,#CFCFCF,#F0F0F0,#E7E7E7' nodata='#EAEAEA' categories='Biden_winner,Biden_leader,Trump_winner,Trump_leader,no-votes,even' \
+  -colorizer name=calcFill colors='#115E9B,#CFCFCF,#AE191C,#CFCFCF,#CFCFCF,#E7E7E7' nodata='#EAEAEA' categories='Biden_winner,Biden_leader,Trump_winner,Trump_leader,no-votes,even' \
   -style fill='calcFill(winner_or_leading)' \
   -o id-field=name format=svg -
   # spatial/mn_2020_general_prez_electoral.svg
